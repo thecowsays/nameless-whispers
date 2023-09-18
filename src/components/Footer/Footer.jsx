01ce4c219cc8801f343791fe3d27114c6284efda
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-09-18 14:29:11 -0400

    added basic files

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>Made with love by</p>
+      <p>&copy;2023 Footer. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
