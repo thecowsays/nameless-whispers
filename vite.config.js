d28a0503a452ec731a07e9b5e972d5c78786d025
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-09-18 16:56:40 -0400

    adjustments to config, etc.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+      generateScopedName: "[local]_[hash:base64:4]",
+    },
+  },
+});
