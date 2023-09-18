7db324df176f3baaafd8393c082ce403221f3b8d
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-09-18 16:57:35 -0400

    WIP Header - links added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,30 @@
-import { SlHome } from "react-icons/sl";
+import { Link, useNavigate } from "react-router-dom";
 
-import "./Header.module.css";
+import { SlHome } from "react-icons/sl";
+import HeaderCSS from "./Header.module.css";
 
 const Header = () => {
+  const navbar = ["File", "Edit", "View", "Login"];
+  const navigate = useNavigate("");
+
   return (
-    <div className="">
-      <SlHome />
-      File
-      Edit
-      View
-      Login
+    <div className={HeaderCSS.container}>
+      {/* <div className="menu-wrapper"> */}
+      <SlHome onClick={() => navigate("/")} />
+      {/* {navbar.map((item) => {
+        <Link to={`/${item}`} className={HeaderCSS.menuLink}>
+          {item}
+        </Link>;
+      })} */}
 
+      {navbar.map((item) => (
+        <Link className={HeaderCSS.menuLink} to={`/${item}`} key={item}>
+          {item}
+        </Link>
+      ))}
 
+      {/* </div> */}
+      {/* <div>Title Goes Here</div> */}
     </div>
   );
 };
