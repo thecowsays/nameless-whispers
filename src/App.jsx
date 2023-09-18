467c8cb6a78ee590edf22cb3fceba36809e005f8
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-09-18 14:29:38 -0400

    added App styles and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Homepage from "./pages/Homepage/Homepage";
+import Footer from "./components/Footer/Footer";
+
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        {/* <Route path="/" element={<Homepage />}/> */}
+        {/* <Route path="/" element={<Homepage />}/> */}
+        {/* <Route path="/" element={<Homepage />}/> */}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
