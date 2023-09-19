fbe40ba366aec39688845035cbbb815b36ec5edf
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-09-19 19:49:18 -0400

    added doc on date feature

--- src/components/NewWhisper/NewWhisper.jsx
+++ src/components/NewWhisper/NewWhisper.jsx
@@ -1,13 +1,14 @@
 import NewWhisperCSS from "./NewWhisper.module.css";
 
 const NewWhisper = () => {
+  // -- display current local date for the user --
   // get date, format and display it (local to user)
   const date = new Date();
   // spell out the month
   const month = date.toLocaleString("default", { month: "long" });
   const day = date.getDate();
   const year = date.getFullYear();
-
+  // final date format as displayed for user
   const todaysDate = `${month} ${day}, ${year}`;
 
   return (
