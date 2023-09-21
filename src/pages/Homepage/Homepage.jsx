ae3cc5db24422aed034b66fab22750941120e213
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-09-20 23:07:24 -0400

    added landing page design

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,11 +1,27 @@
-import NewWhisper from "../../components/NewWhisper/NewWhisper";
+// import NewWhisper from "../../components/NewWhisper/NewWhisper";
 
 import HomepageCSS from "./Homepage.module.css";
 
 const Homepage = () => {
+  // const centerWithMargin = `HomepageCSS.center HomepageCSS.addTopMargin`;
   return (
     <div className={HomepageCSS.container}>
-      <div className={HomepageCSS.contentWrapper}>{<NewWhisper />}</div>
+      <div className={HomepageCSS.contentWrapper}>
+        <div>
+          <h1>Nameless Whispers</h1>
+          <div className={HomepageCSS.mainWrapper}>
+            <p className={HomepageCSS.center}>MAIN MENU</p>
+            <ol>
+              <li>Register / Login</li>
+              <li>Whisper a Thought</li>
+              {/* <li>Listen to Others</li> */}
+              <li>What is this site?</li>
+              <li>Quit</li>
+            </ol>
+          </div>
+          {/* <p></p> */}
+        </div>
+      </div>
     </div>
   );
 };
