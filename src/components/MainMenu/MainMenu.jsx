2cb158561a54ab93f45f499449f92e0dfc83690c
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-09-21 23:31:15 -0400

    init

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -1,7 +1,19 @@
 import MainMenuCSS from "./MainMenu.module.css";
 
 const MainMenu = () => {
-  return <div>MainMenu</div>;
+  return (
+    <div className={MainMenuCSS.mainWrapper}>
+      <p className={MainMenuCSS.title}>Main Menu</p>
+      <ol>
+        {/* Future Feature: Have item 1 change to 'Logout' using Auth */}
+        <li>Signup / Login</li>
+        <li>Whisper a Thought</li>
+        {/* <li>Listen to Others</li> */}
+        <li>Help</li>
+        <li>Quit</li>
+      </ol>
+    </div>
+  );
 };
 
 export default MainMenu;
