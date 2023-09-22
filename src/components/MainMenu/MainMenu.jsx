32b469be46741b54142b399a391a46af4ab7802f
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-09-22 19:48:45 -0400

    updated css class title

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -3,7 +3,7 @@ import MainMenuCSS from "./MainMenu.module.css";
 const MainMenu = () => {
   return (
     <div className={MainMenuCSS.mainWrapper}>
-      <p className={MainMenuCSS.title}>Main Menu</p>
+      <p className="title">Main Menu</p>
       <ol>
         {/* Future Feature: Have item 1 change to 'Logout' using Auth */}
         <li>Signup / Login</li>
