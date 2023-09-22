c9e503ffdc969c1ab0dfee3cfbd81060a00dbf31
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-09-21 21:28:26 -0400

    added new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Homepage from "./pages/Homepage/Homepage";
+import Auth from "./pages/Auth/Auth";
 import Footer from "./components/Footer/Footer";
 
 import "./App.css";
@@ -11,7 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        {/* <Route path="/" element={<Homepage />}/> */}
+        <Route path="/Login" element={<Auth />} />
         {/* <Route path="/" element={<Homepage />}/> */}
         {/* <Route path="/" element={<Homepage />}/> */}
       </Routes>
