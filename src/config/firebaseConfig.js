eca62db576384a30026655f167c9d779c925fc45
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-09-22 20:55:27 -0400

    init Auth

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 // import {useAuthState} from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,3 +23,4 @@ const app = initializeApp(firebaseConfig);
 
 // setup database and export it so we can fetch articles from it
 export const db = getFirestore(app);
+export const auth = getAuth(app);
