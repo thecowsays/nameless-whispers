e930dc67848284f7278b7fd7ecf7a5517905c824
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-09-24 02:24:54 -0400

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Homepage from "./pages/Homepage/Homepage";
 import Auth from "./pages/Auth/Auth";
+import NewWhisper from "./pages/NewWhisper/NewWhisper";
+
 import Footer from "./components/Footer/Footer";
 
 import "./App.css";
@@ -12,8 +14,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/Login" element={<Auth />} />
+        <Route path="/newwhisper" element={<NewWhisper />} />
+        <Route path="/login" element={<Auth />} />
         <Route path="/auth" element={<Auth />} />
+        {/* <Route path="/" element={<Homepage />}/> */}
         {/* <Route path="/" element={<Homepage />}/> */}
       </Routes>
       <Footer />
