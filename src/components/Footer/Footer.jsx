fe90facd0d8daeba07a06c600d5aa43edede19b7
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-09-24 12:46:55 -0400

    added tech icons, copyright, and css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,27 @@
+import { SlHeart } from "react-icons/sl";
+import {
+  SiReact,
+  SiFirebase,
+  SiGithub,
+  SiVisualstudiocode,
+} from "react-icons/si";
+
 import FooterCSS from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <div className={FooterCSS.container}>
-      <p>Made with love by</p>
-      <p>&copy;2023 Footer. All rights reserved.</p>
+      <p className={FooterCSS.icon}>
+        <SiReact style={{ color: "#61DAFB" }} />
+        <SiFirebase style={{ color: "#FFCA28" }} />
+        {/* <SiGithub style={{ color: "#181717" }} /> */}
+        <SiGithub />
+        <SiVisualstudiocode style={{ color: "#007ACC" }} />
+      </p>
+      <p alt="&copy;2023 B.W. All rights reserved.">
+        Made with <SlHeart style={{ color: "#F00", opacity: "100%" }} />
+        by Brandon W.
+      </p>
     </div>
   );
 };
